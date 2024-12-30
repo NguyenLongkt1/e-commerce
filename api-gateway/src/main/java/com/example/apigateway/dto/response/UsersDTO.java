1d21b15f60132b0d22c9a6dfa6cb9f85d438044f
NguyenLongkt1 <nguyenlong99kt@gmail.com>
2024-12-30 08:49:46 +0700

    fix lombok

--- api-gateway/src/main/java/com/example/apigateway/dto/response/UsersDTO.java
+++ api-gateway/src/main/java/com/example/apigateway/dto/response/UsersDTO.java
@@ -1,12 +1,16 @@
 package com.example.apigateway.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsersDTO {
     private Long id;
     private String userName;
